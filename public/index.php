170bfe3dc60e14f7996e8eb90d863624ff970502
Emilien De Macedo <emilien45100@gmail.com>
2019-10-19 13:23:32 +0200

    First Commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/views');
+$twig = new Twig\Environment($loader);
+
+echo $twig->render('base.html.twig' , ['products' => $products]);
