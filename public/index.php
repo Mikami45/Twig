5ef7d3082f8164328ae6be037b5246b2144d1df5
Emilien De Macedo <emilien45100@gmail.com>
2019-10-21 10:01:01 +0200

    first commit

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 $loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/views');
 $twig = new Twig\Environment($loader);
 
-echo $twig->render('base.html.twig' , ['products' => $products]);
+echo $twig->render('index.html.twig' , ['products' => $products]);
